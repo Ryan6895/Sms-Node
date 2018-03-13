43a6445bf91d9fc98432f0af7135f81c34477892
Your NameRyan <Rymaster95@gmail.com>
2018-03-13 19:39:03 -0400

    finished sms Node

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const ejs = require('ejs');
 const Nexmo = require('nexmo');
 const socketio = require('socket.io');
 
+var config = require("./config.js")
+
+//init Nexmo
+const nexmo = new Nexmo(config.nexmo);
+
 // init app 
 const app = express();
 
@@ -25,8 +30,28 @@ app.get('/', (req,res) => {
 
 // catch form submit
 app.post('/', (req, res) =>  {
-    res.send(req.body);
-    console.log(req.body);
+    // res.send(req.body);
+    // console.log(req.body);
+    const number = req.body.number;
+    const text = req.body.text;
+
+    nexmo.message.sendSms(
+        '12014644272', number, text, {type: 'unicode'},
+        (err, responseData) => {
+            if (err){
+                console.log(err);
+            } else {
+                console.dir(responseData);
+                //get data from response
+                const data = {
+                    id: responseData.messages[0]['message-id'],
+                    number: responseData.messages[0]['to']
+                }
+                //emit to client
+                io.emit('smsStatus', data);
+            }
+        }
+    );
 })
 
 // Define port
@@ -34,3 +59,12 @@ const port = 3000;
 
 //start server 
 const server = app.listen(port, () => console.log(`server started on port ${port}`));
+
+// Connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+    console.log('Connected');
+    io.on('disconnect', () => {
+        console.log('Disconnected');
+    })
+})
